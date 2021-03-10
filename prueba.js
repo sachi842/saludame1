52b06451c3f17c034e1a17fcec00f2d347121dcc
Santiago <santiagoriverabotero@gmail.com>
2021-03-10 10:46:38 -0500

    primer commit

---
+++ prueba.js
@@ -0,0 +1,10 @@
+let arreglo = [0, 1, 2, 3 , 4, 5, 6, 7];
+
+arreglo.forEach(myFunction);
+
+function myFunction(item, index) {
+  console.log(item)  
+  if( item > 1 ){
+    console.log(item)
+  }
+}
