d88e866a2503ea77f9eacb5e164f8da9d6b6121c
Santiago <santiagoriverabotero@gmail.com>
2021-05-27 16:03:04 -0500

    saludame 1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 
 
-app.get('/makers/:name', (req, res) => { 
+app.get('/', (req, res) => { 
   res.send('<h1>Hola ' + req.params.name.charAt(0).toUpperCase()+req.params.name.slice(1)+'!</h1>');   
 });
 app.listen(3000, () => console.log('Listening on port 3000!'));
