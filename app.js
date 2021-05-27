e54eac8fa11de8d03bb4f11d44a31e96134a9800
Santiago <santiagoriverabotero@gmail.com>
2021-05-27 16:14:27 -0500

    saludame 1

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 
 
 app.get('/', (req, res) => { 
-  res.send('<h1>Hola ' + req.query.nombre.charAt(0).toUpperCase()+req.query.nombre.slice(1)+'!</h1>');   
+
+  if(req.query.nombre)  
+    res.send('<h1>Hola ' + req.query.nombre.charAt(0).toUpperCase()+req.query.nombre.slice(1)+'!</h1>'); 
+  else
+  res.send('<h1>Hola desconocido!</h1>');   
 });
 app.listen(3000, () => console.log('Listening on port 3000!'));
